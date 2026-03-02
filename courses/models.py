654b9954be8b45da0602c73b9cd36d593213f7cc
Islombek Mirsalimov <mirsalimovislombek9@gmail.com>
2026-03-02 22:54:35 +0500

    add courses app and forms for adding and editing users in admin panel

---
+++ courses/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from users.models import User
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    instructor = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="courses"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    # Add 'role' to the fieldsets (for editing)
+    add_form = CustomUserCreationForm       
+    form = CustomUserChangeForm        
+    model = User
+    list_display = ('username', 'email', 'role', 'is_staff')
     fieldsets = UserAdmin.fieldsets + (
         ('Role Information', {'fields': ('role',)}),
     )
-    # Add 'role' to the list display in the admin table
-    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_staff')
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'role', 'password1', 'password2'),
+        }),
+    )
