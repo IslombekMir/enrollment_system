70ddb96388c598b4eb3126113e88212d71544b92
Islombek Mirsalimov <mirsalimovislombek9@gmail.com>
2026-03-03 00:36:45 +0500

    add core app and lesson and enrollment models

--- courses/admin.py
+++ courses/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Lesson, Enrollment
 
 admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(Enrollment)
--- courses/models.py
+++ courses/models.py
@@ -13,3 +13,53 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Lesson(models.Model):
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="lessons",
+        db_index=True
+    )
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    order = models.PositiveIntegerField()
+    
+    class Meta:
+        ordering = ['order']
+        constraints = [
+	        models.UniqueConstraint(
+	            fields=['course', 'order'],
+	            name='unique_lesson_order_per_course'
+	        )
+	    ]
+
+    def __str__(self):
+        return f"{self.course.title} - {self.title}"
+
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="enrollments",
+        db_index=True
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="enrollments"
+    )
+    enrolled_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['student', 'course'],
+                name='unique_enrollment'
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.student.username} enrolled in {self.course.title}"
